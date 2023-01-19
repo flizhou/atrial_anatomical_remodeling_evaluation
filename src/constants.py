dee1cffcece9eb883e8657147af57369219da6a6
flizhou <flizhou@gmail.com>
2023-01-18 22:27:58 -0500

    Added more constants

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,11 @@
 # constants
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from lightgbm import LGBMClassifier
+from imblearn import over_sampling
+
 class constants(object):
     def __init__(self):
         super().__init__()
@@ -9,3 +15,28 @@ class constants(object):
         self.RESOLUTION = 0.2
         self.CT_RAW = r'data/raw/ct'
         self.CT_PROCESSED = r'data/processed/ct'
+        self.SEED = 81921
+        self.sampling_funcs = {
+            'SMOTE': over_sampling.SMOTE,
+            'ADASYN': over_sampling.ADASYN,
+            'BorderlineSMOTE': over_sampling.BorderlineSMOTE,
+        }
+        self.models = {
+            'LogisticRegression': LogisticRegression, 
+            'RandomForestClassifier': RandomForestClassifier,  
+            'LGBMClassifier': LGBMClassifier,
+            'GaussianProcessClassifier': GaussianProcessClassifier
+        }
+        self.params = {
+            'LogisticRegression': {'solver': 'liblinear'}, 
+            'RandomForestClassifier': {},
+            'GaussianProcessClassifier': {},
+            'LGBMClassifier': {},
+        }
+        self.base_params = {
+            'LogisticRegression': {
+                'solver': 'liblinear',
+                'random_state':self.SEED, 
+                'class_weight': 'balanced',
+            }   
+        }
